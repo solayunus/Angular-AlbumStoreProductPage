6040830d469ebeae673409c332af81e0e1910524
YunusBinKhalid <solayunus19@gmail.com>
2019-10-25 17:32:33 +0100

    Product description update
    upadte on dynamic value for product description

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ProductService  } from "../product.service";
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  albuminfo;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albuminfo = response );
   }
 
 }
