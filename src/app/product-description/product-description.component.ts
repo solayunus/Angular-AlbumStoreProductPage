39394c750e62f19412ec60dd3d6b88822d122035
YunusBinKhalid <solayunus19@gmail.com>
2019-10-25 17:36:28 +0100

    update on albumInfo

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -6,11 +6,11 @@ import {ProductService  } from "../product.service";
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albuminfo;
+  albumInfo;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albuminfo = response );
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response );
   }
 
 }
