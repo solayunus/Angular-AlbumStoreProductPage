b2955efbf45d1df8dc2d2004847d31fc169ebd5f
YunusBinKhalid <solayunus19@gmail.com>
2019-10-26 20:33:53 +0100

    Product list update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,9 +3,13 @@ import {Http , Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from "./album";
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/product.json'
+
+
   constructor(private _http: Http) {
      
   }
@@ -13,5 +17,8 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) =>
       <Album>response.json());
   } 
+  getProducts():Observable<Product[]> {
+    return this._http.get(this._productsUrl).map( response => <Product[]> response.json());
+  }
 }
 
